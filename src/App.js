e79e4fe3ccfde95758c907913a561bb52028b283
tommy <doviethung.5409@gmail.com>
2025-06-30 10:03:55 +0700

    set-up-project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+const App = () => {
+ 
+
+  return (
+    <div className="app-container">
+      hello world
+     
+    </div>
+  );
+}
+
+export default App;
