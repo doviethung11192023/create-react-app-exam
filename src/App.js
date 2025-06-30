b30a41043cb7fce3e5ab3b775bf322cc385909d6
tommy <doviethung.5409@gmail.com>
2025-06-30 10:24:57 +0700

    make the navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import Header from './component/Header/header';
 const App = () => {
  
 
   return (
     <div className="app-container">
-      hello world
+      <Header />
      
     </div>
   );
