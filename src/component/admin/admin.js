4b14c40a83a16bcef8fec6f457290d8bf15b33e9
tommy <doviethung.5409@gmail.com>
2025-07-20 15:11:00 +0700

    validate email and add toast

--- src/component/admin/admin.js
+++ src/component/admin/admin.js
@@ -1,6 +1,7 @@
 import SideBar from "./SideBar";
 import "./Admin.scss";
 import { Outlet } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 const Admin = (props) => {
   return (
     <div className="admin-container">
@@ -15,6 +16,18 @@ const Admin = (props) => {
         <div className="admin-header">header-admin</div>
         <div className="admin-body">header-content</div> */}
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 };
--- src/component/admin/content/ModalCreateUser.js
+++ src/component/admin/content/ModalCreateUser.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FcPlus } from "react-icons/fc";
+import { ToastContainer, toast } from "react-toastify";
 const ModalCreateUser = (props) => {
   const { show, setShow } = props;
   //const [show, setShow] = useState(false);
@@ -28,24 +29,50 @@ const ModalCreateUser = (props) => {
     setImagePreview(URL.createObjectURL(file));
     if (file) {
       setImagePreview(URL.createObjectURL(file));
-      setImage(file);
+      console.log("file: ", file);
+      setImage(URL.createObjectURL(file));
     } else {
       //setImagePreview(null);
     }
   };
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
   const handleSubmitModal = async () => {
-    alert("Submit form");
+    //alert("Submit form");
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      toast.error("Email is not valid");
+      return;
+    }
+    if (!password || !username) {
+      toast.error("Please fill in all fields");
+      return;
+    }
+    console.log("image: ", image);
     const data = new FormData();
     data.append("email", email);
     data.append("password", password);
     data.append("username", username);
     data.append("role", role);
     data.append("image", image);
+    console.log("data: ", data);
     let response = await axios.post(
       "http://localhost:8081/api/v1/participant",
       data
     );
     console.log("response: ", response);
+    if (response.data && response.data.EC === 0) {
+      toast.success(response.data.EM);
+      handleClose();
+    }
+    if (response.data && response.data.EC !== 0) {
+      toast.error(response.data.EM);
+    }
   };
 
   return (
